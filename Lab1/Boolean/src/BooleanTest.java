f85363015d4c2fefbb8897972fa420aff660c0e5
Varsha <varsha_raghav@hotmail.com>
2018-01-07 11:42:22 -0500

    Lab1
    JUnit test case to test the class Boolean. (API :
    https://www.eecs.yorku.ca/course_archive/2017-18/W/4315/lab1/)

---
+++ Lab1/Boolean/src/BooleanTest.java
@@ -0,0 +1,80 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BooleanTest {
+		
+	// attributes
+			/**
+			 * This field is a Boolean object representing the primitive value true.
+			 */
+			@Test
+			public void testTrueAttribute() {
+				Assert.assertEquals(true, lab.Boolean.TRUE.booleanValue());
+			}
+
+			// constructor
+			/**
+			 * Create a Boolean object representing the value of the argument value.
+			 */
+			@Test
+			public void testBoolean(){
+				try {
+					new lab.Boolean(true);
+					new lab.Boolean(false);
+					
+				}catch(Exception e) {
+					Assert.fail(e.getMessage());
+				}
+			}
+
+			// functions
+			/**
+			 * Return the primitive boolean value of this Boolean object.
+			 */
+			@Test
+			public void testbooleanValue() {
+				lab.Boolean b = new lab.Boolean(true);
+				Assert.assertEquals(true, b.booleanValue());	
+				lab.Boolean bool = null;
+				Assert.assertNull(bool);
+			}
+
+			/**
+			 * 
+			 * @throws IllegalArgumentException
+			 *             - if other is null.
+			 */
+			@Test(expected=IllegalArgumentException.class)
+			public void compareToException() throws IllegalArgumentException {
+				lab.Boolean.TRUE.compareTo(null);
+			}
+			
+			
+			@Test
+			public void compareTo() {
+				try {
+					int compareResult;
+					
+					//0 if both Booleans represent the same value, 
+					lab.Boolean one = new lab.Boolean(true);
+					lab.Boolean two = new lab.Boolean(true);
+					compareResult = one.compareTo(two);
+					Assert.assertEquals(0, compareResult);
+					
+					//a positive number if this Boolean represents true and the other false, 
+					two = new lab.Boolean(false);
+					compareResult = one.compareTo(two);
+					Assert.assertTrue(compareResult > 0);
+					
+					// and a negative number if this Boolean represents false and the other true.
+					compareResult = two.compareTo(one);
+					Assert.assertTrue(compareResult < 0);
+					
+					
+					
+				}catch(Exception e) {
+					Assert.fail(e.getMessage());
+				}
+			}
+
+}
